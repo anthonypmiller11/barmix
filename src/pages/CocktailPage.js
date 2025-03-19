5f65c89a09781e099a97a81c30404798e6d5027f
Tony Miller <82892112+anthonypmiller11@users.noreply.github.com>
2025-03-19 14:18:38 -0600

    Update CocktailPage.js

--- src/pages/CocktailPage.js
+++ src/pages/CocktailPage.js
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import { useTitle } from "../hooks/useTitle";
 import { fetchCocktailDetails } from "../app/features/detailsSlice";
 import { AboutIngredient, Title, Modal } from "../components";
-import { CocktailInfo, Instructions } from "../containers";
+import { CocktailInfo, Instructions, VideoTutorial } from "../containers";
 import { hideIngredientModal } from "../app/features/modalSlice";
 import AnimateRoute from "../containers/layout/AnimateRoute";
 
@@ -13,23 +13,29 @@ const CocktailPage = () => {
   const cocktail = useSelector((state) => state.details.cocktail);
   const loading = useSelector((state) => state.details.loading);
   const { id } = useParams();
+
   const showModal = useSelector((state) => state.modal.showIngredientModal);
 
-  useTitle(`${cocktail?.strDrink || "Loading"} | Cocktails`, loading);
+  useTitle(`${cocktail.drink} | Cocktails`, loading);
 
   const onCloseModal = () => {
     dispatch(hideIngredientModal());
   };
 
   useEffect(() => {
-    dispatch(fetchCocktailDetails(id));
-  }, [id, dispatch]);
+    const promise = dispatch(fetchCocktailDetails(id));
 
+    return () => {
+      promise.abort();
+    };
+  }, [id, dispatch]);
   return (
     <AnimateRoute>
       <CocktailInfo cocktail={cocktail} loading={loading} />
-      <Title title="Instructions" />
+      <Title title={"Instructions"} />
       <Instructions cocktail={cocktail} loading={loading} />
+      <Title title={"Video Guide"} />
+      <VideoTutorial cocktail={cocktail} loading={loading} />
       <Modal onCloseModal={onCloseModal} show={showModal}>
         <AboutIngredient />
       </Modal>
