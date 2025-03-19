64a7680af30283b628bad3d816f754ec2c6981a2
Tony Miller <82892112+anthonypmiller11@users.noreply.github.com>
2025-03-19 13:29:31 -0600

    Update Cocktails.js

--- src/containers/home/Cocktails.js
+++ src/containers/home/Cocktails.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchCocktails } from "../../app/features/cocktailsSlice";
+import { fetchCocktails, fetchByFirstLetter, onLetterClick } from "../../app/features/cocktailsSlice";
+import SelectLetter from "../../components/home/SelectLetter";
+import { Link } from "react-router-dom";
 
 const Cocktails = () => {
   const dispatch = useDispatch();
@@ -11,14 +13,28 @@ const Cocktails = () => {
     dispatch(fetchCocktails());
   }, [dispatch]);
 
+  const handleLetterFilter = (letter) => {
+    dispatch(fetchByFirstLetter(letter));
+    dispatch(onLetterClick(cocktails.filter(cocktail => 
+      cocktail.strDrink.toUpperCase().startsWith(letter)
+    )));
+  };
+
   return (
-    <div>
-      {loading === "idle" || loading === "pending" ? (
-        <p>Loading...</p>
+    <div className="w-full flex flex-col justify-start items-center px-4 md:px-6 lg:px-20 my-6 md:my-8 lg:my-12">
+      <SelectLetter onLetterFilter={handleLetterFilter} />
+      {loading === HTTP_STATUS.FULFILLED ? (
+        <div className="w-full flex flex-col gap-2">
+          {cocktails.map((cocktail) => (
+            <Link key={cocktail.idDrink} to={`/cocktail/${cocktail.idDrink}`}>
+              <div className="text-app-cadet font-app-text text-[14px] md:text-[16px] lg:text-[18px] hover:text-app-flame">
+                {cocktail.strDrink}
+              </div>
+            </Link>
+          ))}
+        </div>
       ) : (
-        cocktails.map((cocktail) => (
-          <div key={cocktail.idDrink}>{cocktail.strDrink}</div>
-        ))
+        <p>Loading...</p>
       )}
     </div>
   );
