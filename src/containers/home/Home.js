9e43e0f2195e0ac7de46191c857d7ead5f493bb1
Tony Miller <82892112+anthonypmiller11@users.noreply.github.com>
2025-03-19 13:50:49 -0600

    Update Home.js

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
   return (
     <div>
       <Title title="Welcome to BarMix" />
-      {loading === HTTP_STATUS.FULFILLED && randomCocktail ? (
+      {loading === "fulfilled" && randomCocktail ? (
         <div className="w-full flex flex-col justify-center items-center px-4 md:px-6 lg:px-20 my-6 md:my-8 lg:my-12">
           <h2 className="text-app-cadet font-app-heading text-[20px] md:text-[24px] lg:text-[28px] text-center">
             {randomCocktail.strDrink}
