3874c95490946d440fbcefd64ec466fc52e3d00b
Tony Miller <82892112+anthonypmiller11@users.noreply.github.com>
2025-03-19 11:44:22 -0600

    Update aboutIngredientSlice.js

--- src/app/features/aboutIngredientSlice.js
+++ src/app/features/aboutIngredientSlice.js
@@ -1,47 +1,28 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-import { API_BASE_URL, HTTP_STATUS } from "../utils/constants";
-import { organizeIngredient } from "../utils/helpers";
+import React from "react";
+import { useSelector } from "react-redux";
+import { HTTP_STATUS } from "../../app/utils/constants";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
-export const fetchIngredientDetails = createAsyncThunk(
-  "aboutIngredient/fetchIngredientDetails",
-  async (ingredient, { signal }) => {
-    const source = axios.CancelToken.source();
-    signal.addEventListener("abort", () => {
-      source.cancel();
-    });
-    const response = await axios.get(
-      `${API_BASE_URL}/search.php?i=${ingredient}`,
-      {
-        cancelToken: source.token,
-      }
-    );
-    return organizeIngredient(response.data.ingredients[0]);
-  }
-);
+const AboutIngredient = () => {
+  const loading = useSelector((state) => state.ingredient.loading);
+  const currentIngredient = useSelector((state) => state.ingredient.currentIngredient);
 
-const initialState = {
-  ingredient: {},
-  loading: null,
-  error: null,
+  return (
+    <div className="w-full flex flex-col justify-start items-center">
+      <p className="font-app-quote text-app-cadet text-[20px] md:text-[24px] lg:text-[28px] xl:text-[32px] text-center mb-2 md:mb-4">
+        {loading === HTTP_STATUS.FULFILLED && currentIngredient ? currentIngredient.strIngredient1 : "Loading..."}
+      </p>
+      {loading === HTTP_STATUS.FULFILLED && currentIngredient && (
+        <LazyLoadImage
+          src={currentIngredient.imageMedium} // Medium for modal
+          alt={currentIngredient.strIngredient1}
+          className="w-40 h-40 md:w-48 md:h-48 object-cover rounded-full mb-4"
+          placeholder={<div className="w-40 h-40 md:w-48 md:h-48 bg-gray-300 rounded-full animate-pulse"></div>}
+          onError={(e) => (e.target.src = "/images/ingredients/default-medium.png")}
+        />
+      )}
+    </div>
+  );
 };
 
-export const aboutIngredientSlice = createSlice({
-  name: "aboutIngredient",
-  initialState: initialState,
-  extraReducers: {
-    [fetchIngredientDetails.pending]: (state) => {
-      state.loading = HTTP_STATUS.PENDING;
-    },
-    [fetchIngredientDetails.fulfilled]: (state, { payload }) => {
-      state.loading = HTTP_STATUS.FULFILLED;
-      state.ingredient = payload;
-    },
-    [fetchIngredientDetails.rejected]: (state, action) => {
-      state.loading = HTTP_STATUS.REJECTED;
-      state.error = action.error.message;
-    },
-  },
-});
-
-export default aboutIngredientSlice.reducer;
+export default AboutIngredient;
