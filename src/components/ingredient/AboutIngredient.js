c8efa1b364a6fcb07a116ccb41496fec53f63d19
Tony Miller <82892112+anthonypmiller11@users.noreply.github.com>
2025-03-19 12:28:57 -0600

    Update AboutIngredient.js

--- src/components/ingredient/AboutIngredient.js
+++ src/components/ingredient/AboutIngredient.js
@@ -1,23 +1,17 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { HTTP_STATUS } from "../app/utils/constants";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const AboutIngredient = () => {
-  const loading = useSelector((state) => state.ingredient.loading);
-  const ingredient = useSelector((state) => state.ingredient.currentIngredient);
-
+  const currentIngredient = useSelector((state) => state.aboutIngredient.ingredient);
   return (
-    <div className="w-full flex flex-col justify-center items-center gap-4 p-4 md:p-6">
-      <p className="text-app-cadet font-app-quote text-[18px] md:text-[20px] lg:text-[24px] text-center">
-        {loading === HTTP_STATUS.FULFILLED ? ingredient.strIngredient1 : "Loading..."}
-      </p>
-      {loading === HTTP_STATUS.FULFILLED && (
+    <div>
+      <p>{currentIngredient?.strIngredient1 || "Loading..."}</p>
+      {currentIngredient && (
         <LazyLoadImage
-          className="w-32 h-32 md:w-40 md:h-40 rounded-full object-cover drop-shadow-md"
-          src={ingredient.image}
-          alt={ingredient.strIngredient1}
-          placeholder={<div className="loading animate-loading w-32 h-32 md:w-40 md:h-40 rounded-full"></div>}
+          src={currentIngredient.imageMedium}
+          alt={currentIngredient.strIngredient1}
+          className="w-40 h-40 md:w-48 md:h-48 object-cover rounded-full mb-4"
         />
       )}
     </div>
