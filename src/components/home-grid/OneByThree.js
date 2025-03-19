70f6c4fbdfa00dac1c8cbc36279e951a9980cc11
Tony Miller <82892112+anthonypmiller11@users.noreply.github.com>
2025-03-19 14:01:50 -0600

    Update OneByThree.js

--- src/components/home-grid/OneByThree.js
+++ src/components/home-grid/OneByThree.js
@@ -1,69 +1,63 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import { HTTP_STATUS } from "../../app/utils/constants";
-import { ImagePlaceHolder } from "../../assets/";
-import { Alcoholic, Favorite, LinkButton } from "../../components";
+import { fromBelow } from "../../app/utils/animationsHelper";
+import { LinkButton } from "../buttons";
 
-const OneByThree = ({ cocktail, loading }) => {
-  const { id, drink, image, category, alcoholic } = cocktail;
+const OneByThree = ({ cocktail, loading, link }) => {
   return (
-    <div
-      className={`bg-white h-full w-full rounded-[5px] drop-shadow-lg group overflow-hidden relative hover:ring-1 hover:ring-white cursor-default ${
-        loading === HTTP_STATUS.FULFILLED && " from-bottom"
-      }`}
+    <motion.div
+      variants={fromBelow}
+      initial="initial"
+      whileInView="animate"
+      viewport={{ once: true }}
+      transition={{
+        ease: "easeInOut",
+        duration: 0.3,
+        delay: 0.1,
+      }}
+      className="relative rounded-xl drop-shadow-xl h-max group ring-1 ring-white"
     >
-      <div className="rounded-[5px] overflow-hidden">
-        <div className="p-[6px] md:p-2 xl:p-[10px] relative">
-          {loading !== HTTP_STATUS.FULFILLED && (
-            <div className="loading animate-loading aspect-[4/3] w-full rounded-[5px]"></div>
-          )}
-          {loading === HTTP_STATUS.FULFILLED && (
-            <>
-              <LazyLoadImage
-                placeholder={<div className="loading animate-loading aspect-[4/3] w-full rounded-[5px]"></div>}
-                className="aspect-[4/3] w-full object-cover rounded-[5px] group-hover:scale-[1.85] group-hover:translate-y-4 group-hover:blur-[2px] basic-transition"
-                src={image ?? ImagePlaceHolder}
-                alt={drink ?? "Cocktail Image"}
-              />
-              <Alcoholic alcoholic={alcoholic} />
-              <Favorite id={id} cocktail={cocktail} />
-            </>
-          )}
-        </div>
-        <div className="px-1">
-          {loading !== HTTP_STATUS.FULFILLED && (
-            <div className="flex flex-col justify-start items-start mx-1">
-              <p className="loading animate-loading rounded-md text-slate-100 h-[20px] w-full mb-1"></p>
-              <p className="loading animate-loading rounded-[5px] text-slate-100 h-[16px] w-full mb-2 xl:mb-[10px]"></p>
-            </div>
-          )}
-          {loading === HTTP_STATUS.FULFILLED && (
-            <div className="pb-1">
-              <p className="text-[12px] md:text-[13px] lg:text-[14px] xl:text-[15px] text-center font-app-text text-app-flame truncate leading-5">
-                {drink ?? "Cocktail"}
-              </p>
-              <p className="text-[13x] md:text-[14px] lg:text-[15px] xl:text-[16px] text-center font-app-heading font-bold text-app-cadet truncate leading-5">
-                {category ?? "Category"}
-              </p>
-            </div>
-          )}
-        </div>
+      <div className="relative">
+        {loading ? (
+          <div className="loading animate-loading rounded-xl w-full aspect-[4/3]"></div>
+        ) : (
+          <LazyLoadImage
+            className="w-full h-auto rounded-xl object-cover drop-shadow-md md:drop-shadow-lg aspect-[4/3]"
+            src={cocktail.image}
+            alt={cocktail.drink}
+            placeholder={
+              <div className="loading animate-loading rounded-xl w-full aspect-[4/3]"></div>
+            }
+          />
+        )}
       </div>
-      {loading === HTTP_STATUS.FULFILLED && (
-        <div className="z-[2] pt-5 rounded-[5px] h-full w-full flex justify-center items-center overflow-hidden absolute top-0 left-0 right-0">
-          <div className="relative w-full flex justify-center items-center">
-            <div className="px-3 pb-2 flex flex-col items-center justify-center scale-0 group-hover:scale-100 absolute -bottom-48 group-hover:bottom-1 lg:group-hover:bottom-2 group-hover:delay-[150ms] group-hover:duration-500 duration-150">
-              <p className="text-[12px] md:text-[13px] lg:text-[14px] xl:text-[15px] text-center font-app-text text-white leading-5">
-                {drink ?? "Cocktail"}
-              </p>
-            </div>
-            <div className="flex items-center justify-center px-8 scale-0 group-hover:scale-100 absolute -bottom-48 group-hover:-bottom-[20px] lg:group-hover:-bottom-[24px] group-hover:delay-[450ms] group-hover:duration-500 duration-150">
-              <LinkButton link={`/cocktails/${id}`} text="View Recipe" />
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
+      <div className="absolute bottom-0 w-full h-1/2 bg-gradient-to-t from-app-cadet/80 to-transparent rounded-b-xl"></div>
+      <div className="absolute bottom-0 w-full flex flex-col justify-end items-center px-2 py-2">
+        {loading ? (
+          <div className="loading animate-loading rounded-xl w-3/4 h-5 md:h-6 mb-2"></div>
+        ) : (
+          <p className="text-white font-app-heading text-[12px] md:text-[13px] lg:text-[15px] xl:text-[16px] text-center mb-2">
+            {cocktail.drink}
+          </p>
+        )}
+        {loading ? (
+          <div className="loading animate-loading rounded-xl w-1/2 h-4 md:h-5 mb-4"></div>
+        ) : (
+          <p className="text-white font-app-main text-[10px] md:text-[11px] lg:text-[12px] xl:text-[13px] text-center mb-4">
+            {cocktail.alcoholic}
+          </p>
+        )}
+        {loading ? (
+          <div className="loading animate-loading rounded-xl w-1/2 h-8 md:h-10"></div>
+        ) : (
+          <Link to={link}>
+            <LinkButton text="View Recipe" />
+          </Link>
+        )}
+      </div>
+    </motion.div>
   );
 };
 
