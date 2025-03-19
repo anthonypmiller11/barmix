8fa2919db5f2b8d124e69cd517ff998f49783b44
Tony Miller <82892112+anthonypmiller11@users.noreply.github.com>
2025-03-19 13:23:02 -0600

    Update constants.js

--- src/app/utils/constants.js
+++ src/app/utils/constants.js
@@ -1,16 +1,15 @@
-export const API_BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1";
-export const YOUTUBE_API_BASE_URL = "https://youtube.googleapis.com/youtube/v3";
+export const HTTP_STATUS = {
+  PENDING: "pending",
+  FULFILLED: "fulfilled",
+  REJECTED: "rejected",
+  IDLE: "idle",
+};
 
-export const HTTP_STATUS = Object.freeze({
-  PENDING: "PENDING",
-  FULFILLED: "FULFILLED",
-  REJECTED: "REJECTED",
-});
-
-export const SCREEN_SIZE = Object.freeze({
+export const SCREEN_SIZE = {
+  XS: 345,
   SM: 640,
   MD: 768,
   LG: 1050,
   XL: 1380,
   XXL: 1536,
-});
+};
