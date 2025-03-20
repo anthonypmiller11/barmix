586110a38a3c28f28a2bbed9d7ea7f1cbe4a7bca
Tony Miller <82892112+anthonypmiller11@users.noreply.github.com>
2025-03-19 20:18:06 -0600

    Update IngredientDetailsTile.js

--- src/components/ingredient/IngredientDetailsTile.js
+++ src/components/ingredient/IngredientDetailsTile.js
@@ -3,21 +3,21 @@ import { HTTP_STATUS } from "../../app/utils/constants";
 
 const IngredientDetailsTile = ({loading, title, text}) => {
   return (
-    <div className="flex flex-col justify-center items-center my-[7px] md:my-2 px-2 md:px-4 w-full">
+    <div className="flex flex-col justify-center items-center my-[10px] md:my-2 px-2 md:px-4 w-full">
       {loading !== HTTP_STATUS.FULFILLED && (
         <div className="w-full space-y-1">
-          <p className="loading animate-loading text-[16px] text-slate-100 text-center truncate leading-5">
+          <p className="loading animate-loading text-[17px] text-slate-100 text-center truncate leading-5">
             ...
           </p>
         </div>
       )}
 
       {loading === HTTP_STATUS.FULFILLED && (
-        <p className="text-[14px] md:text-[15px] lg:text-[16px] font-app-text text-app-cadet text-center">
+        <p className="text-[16px] md:text-[17px] lg:text-[18px] font-app-text text-app-cadet text-center">
           {text}
         </p>
       )}
-      <p className="text-[13px] md:text-[14px] lg:text-[15px] font-app-main text-app-olivine text-center tracking-wider">
+      <p className="text-[15px] md:text-[16px] lg:text-[17px] font-app-main text-app-olivine text-center tracking-wider">
         {title}
       </p>
     </div>
