0e979a0958d9906985855d69544fcacecf3d9336
Tony Miller <82892112+anthonypmiller11@users.noreply.github.com>
2025-03-19 20:19:40 -0600

    Update InfoCard.js

--- src/components/cocktail/InfoCard.js
+++ src/components/cocktail/InfoCard.js
@@ -22,10 +22,10 @@ const InfoCard = ({ title, data, loading }) => {
       {loading === HTTP_STATUS.FULFILLED && (
         <>
           <div className="flex flex-col justify-center items-center rounded-lg border-[3px] border-dashed w-full h-full px-4 lg:px-8 py-2 cursor-default">
-            <p className="text-[11px] md:text-[13px] lg:text-[14px] font-app-text text-app-cadet text-center">
+            <p className="text-[15px] md:text-[16px] lg:text-[17px] font-app-text text-app-cadet text-center">
               {data}
             </p>
-            <p className="text-[9px] md:text-[10px] lg:text-[11px] font-app-main text-app-olivine text-center tracking-wider">
+            <p className="text-[13px] md:text-[14px] lg:text-[15px] font-app-main text-app-olivine text-center tracking-wider">
               {title}
             </p>
           </div>
