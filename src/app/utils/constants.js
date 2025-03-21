a6e8f58117d8834f6710643b495e0e84e70bd662
Tony Miller <82892112+anthonypmiller11@users.noreply.github.com>
2025-03-20 18:42:02 -0600

    Update constants.js

--- src/app/utils/constants.js
+++ src/app/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1";
+export const API_BASE_URL = "/data";  // Points to your local JSON folder 
 export const YOUTUBE_API_BASE_URL = "https://youtube.googleapis.com/youtube/v3";
 
 export const HTTP_STATUS = Object.freeze({
