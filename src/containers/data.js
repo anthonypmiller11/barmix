ef1c3540082832f76aeefaf5f3f85ef799372703
Tony Miller <82892112+anthonypmiller11@users.noreply.github.com>
2025-03-21 08:17:19 -0600

    Create data.js

---
+++ src/containers/data.js
@@ -0,0 +1,22 @@
+// src/containers/data.js
+// Data to make Cocktails.js work on Netlify
+export const FeaturedCocktails = [
+  { 
+    id: 1, 
+    name: "Mojito", 
+    description: "Minty refreshing drink", 
+    image: "/images/cocktails/mojito.jpg" 
+  },
+  { 
+    id: 2, 
+    name: "Margarita", 
+    description: "Tequila citrus blend", 
+    image: "/images/cocktails/margarita.jpg" 
+  },
+  { 
+    id: 3, 
+    name: "Old Fashioned", 
+    description: "Classic whiskey cocktail", 
+    image: "/images/cocktails/oldfashioned.jpg" 
+  }
+];
