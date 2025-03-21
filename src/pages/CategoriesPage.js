3e63d8142c675f68e87ff95cad015890fb3a0e0c
Tony Miller <82892112+anthonypmiller11@users.noreply.github.com>
2025-03-21 06:49:16 -0600

    Update CategoriesPage.js

--- src/pages/CategoriesPage.js
+++ src/pages/CategoriesPage.js
@@ -1,3 +1,4 @@
+// src/pages/CategoriesPage.js
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useSearchParams } from "react-router-dom";
@@ -12,17 +13,15 @@ import { categoryTypes } from "../app/utils/data";
 const CategoriesPage = () => {
   const dispatch = useDispatch();
   const [searchParams, setSearchParams] = useSearchParams();
+  const type = searchParams.get("id") ?? "0";
 
-  const type = searchParams.get("id") ?? 0;
-
-  const types = categoryTypes;
   const cocktails = useSelector((state) => state.category.cocktails);
   const loading = useSelector((state) => state.category.loading);
   const error = useSelector((state) => state.category.error);
 
   const [selectedType, setSelectedType] = useState(type);
 
-  useTitle(`${types?.[selectedType] ?? "Error"} | Cocktails`);
+  useTitle(`${categoryTypes?.[selectedType] ?? "Error"} | Cocktails`);
   const size = useWindowSize();
 
   const onChangeType = (index) => {
@@ -33,37 +32,30 @@ const CategoriesPage = () => {
 
   useEffect(() => {
     const promise = dispatch(fetchByCategory(selectedType));
-
-    return () => {
-      promise.abort();
-    };
+    return () => promise.abort();
   }, [dispatch, selectedType, type]);
 
   return (
     <AnimateRoute>
       <Title title="Select From Your Favorite Category" />
       <div className="bg-image flex justify-center gap-3 md:gap-5 lg:gap-6 flex-wrap mt-7 mb-8 md:mt-10 md:mb-12 py-6 md:py-8 lg:py-10 px-2 md:px-20 lg:px-28">
-        {types.map((category, index) => {
-          return (
-            <div
-              key={index}
-              className={`rounded-md px-[12px] md:px-4 lg:px-6 py-[14px] md:py-[16px] lg:py-2 drop-shadow-lg cursor-pointer group md:hover:scale-110 basic-transition ${
-                index === Number(selectedType) ? "bg-app-flame" : "bg-white"
+        {categoryTypes.map((category, index) => (
+          <div
+            key={index}
+            className={`rounded-md px-[12px] md:px-4 lg:px-6 py-[14px] md:py-[16px] lg:py-2 drop-shadow-lg cursor-pointer group md:hover:scale-110 basic-transition ${
+              index === Number(selectedType) ? "bg-app-flame" : "bg-white"
+            }`}
+            onClick={() => onChangeType(index)}
+          >
+            <p
+              className={`text-sm md:text-base lg:text-lg font-app-text ${
+                index === Number(selectedType) ? "text-white" : "text-app-cadet"
               }`}
-              onClick={() => onChangeType(index)}
             >
-              <p
-                className={`text-app-cadet text-sm md:text-base lg:text-lg font-app-text ${
-                  index === Number(selectedType)
-                    ? "text-white"
-                    : "text-app-cadet"
-                }`}
-              >
-                {category}
-              </p>
-            </div>
-          );
-        })}
+              {category}
+            </p>
+          </div>
+        ))}
       </div>
       <div className="px-[12vw] md:px-[13vw] lg:px-[14vw] overflow-hidden">
         <CocktailsGrid
