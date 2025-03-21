05f4bb4f983ab6ed8a83e4797ad2ba4a91db5c21
Tony Miller <82892112+anthonypmiller11@users.noreply.github.com>
2025-03-21 10:44:49 -0600

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,45 +1,45 @@
-import { configureStore } from "@reduxjs/toolkit";
-import storage from 'redux-persist/lib/storage';
-import { persistReducer } from 'redux-persist';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 import cocktailsReducer from "./features/cocktailsSlice";
 import randomReducer from "./features/randomSlice";
 import detailsReducer from "./features/detailsSlice";
 import youtubeReducer from "./features/youtubeSlice";
-import alcoholicReducer from "./features/alcoholicSlice";
-import categoryReducer from "./features/categorySlice";
-import glassReducer from "./features/glassSlice";
 import favoriteReducer from "./features/favoriteSlice";
-import ingredientReducer from "./features/ingredientSlice";
-import aboutIngredientReducer from "./features/aboutIngredientSlice";
-import fetchByIngredientReducer from "./features/fetchByIngredientSlice";
-import modalReducer from "./features/modalSlice";
-import searchReducer from "./features/searchSlice";
+import popularReducer from "./features/popularSlice"; // Hypothetical
+// ... other reducers
 
 const persistConfig = {
-  key: 'favorites',
-  storage
+  key: "root",
+  storage,
+  whitelist: ["favorite"], // Only persist the favorite slice
 };
 
-const persistedFavoriteReducer = persistReducer(persistConfig, favoriteReducer);
+const rootReducer = combineReducers({
+  cocktails: cocktailsReducer,
+  random: randomReducer,
+  details: detailsReducer,
+  youtube: youtubeReducer,
+  favorite: favoriteReducer,
+  popular: popularReducer,
+  // ... other reducers
+});
 
-export default configureStore({
-  reducer: {
-    random: randomReducer,
-    cocktails: cocktailsReducer,
-    details: detailsReducer,
-    alcoholic: alcoholicReducer,
-    category: categoryReducer,
-    glass: glassReducer,
-    youtube: youtubeReducer,
-    ingredient: ingredientReducer,
-    aboutIngredient: aboutIngredientReducer,
-    fetchByIngredient: fetchByIngredientReducer,
-    modal: modalReducer,
-    search: searchReducer,
-    favorite: persistedFavoriteReducer,
-  },
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST"],
+      },
     }),
+});
+
+export const persistor = persistStore(store);
+
+// Clear the persisted state on app load
+persistor.purge().then(() => {
+  console.log("Persisted state cleared");
 });
