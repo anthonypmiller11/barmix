74038d869ac787fae7b723508c7af8273dcf4fc2
Tony Miller <82892112+anthonypmiller11@users.noreply.github.com>
2025-03-20 20:36:33 -0600

    Update AboutIngredient.js

--- src/components/ingredient/AboutIngredient.js
+++ src/components/ingredient/AboutIngredient.js
@@ -15,8 +15,8 @@ const AboutIngredient = () => {
 
   const onClickHandler = () => {
     dispatch(hideIngredientModal());
-    navigate(`/ingredients/${ingredient?.name}`)
-  }
+    navigate(`/ingredients/${ingredient?.name}`);
+  };
 
   return (
     <div className="flex flex-col justify-center items-center cursor-default">
@@ -24,18 +24,21 @@ const AboutIngredient = () => {
         Ingredient Details
       </h1>
       <div className="grid grid-rows-9 grid-cols-1 md:grid-cols-9 md:grid-rows-1 mt-2 justify-center gap-2 md:gap-4">
+        {/* Ingredient Image with Local Path */}
         <div className="col-start-1 row-start-1 row-span-4 md:row-span-1 md:col-start-1 md:col-span-4 overflow-hidden">
           <div className="p-[12px] min-h-[150px] md:p-2 h-max md:h-full w-auto bg-app-cadet/50 rounded-[8px] flex justify-center items-center">
-            {loading === HTTP_STATUS.FULFILLED && (
+            {loading === HTTP_STATUS.FULFILLED && ingredient?.name && (
               <LazyLoadImage
                 className="aspect-square w-full object-cover rounded-[5px] max-w-[75vw] md:max-w-[200px] scale-90 md:scale-110"
-                src={`https://www.thecocktaildb.com/images/ingredients/${ingredient.name}-Medium.png`}
+                src={`/images/ingredients/${ingredient.name.replace(/\s+/g, "_")}-medium.png`}
                 alt={ingredient.name}
                 placeholder={<div className="loading animate-loading aspect-square w-full rounded-[5px] max-w-[75vw] md:max-w-[200px]"></div>}
               />
             )}
           </div>
         </div>
+
+        {/* Ingredient Details Section */}
         <div className="col-start-1 row-start-5 row-span-5 md:row-span-1 md:col-start-5 md:col-span-5 md:row-start-1 flex py-2 px-6 md:px-3 flex-col justify-center items-center w-auto min-w-[200px] rounded-[5px] border-[3px] border-dashed">
           <IngredientDetailsTile
             loading={loading}
