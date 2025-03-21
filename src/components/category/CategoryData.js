b650bb24688730024e96a6eab3bad0e29f824d5c
Tony Miller <82892112+anthonypmiller11@users.noreply.github.com>
2025-03-20 23:52:46 -0600

    Create CategoryData.js

---
+++ src/components/category/CategoryData.js
@@ -0,0 +1,58 @@
+// src/components/category/categoryData.js
+
+const categoryData = {
+  Spirit: [
+    "Vodka",
+    "Tequila",
+    "Whiskey",
+    "Gin",
+    "Rum",
+    "Champagne",
+    "Brandy"
+  ],
+  Strengths: [
+    "Popular",
+    "Light",
+    "Medium",
+    "Strong",
+    "Nonalcoholic",
+    "Trendy",
+    "Exotic"
+  ],
+  Flavor: [
+    "Sweet",
+    "Sour",
+    "Bitter",
+    "Fruity",
+    "Spicy",
+    "Smoky",
+    "Creamy"
+  ],
+  Style: [
+    "Classic",
+    "Tropical",
+    "Sparkling",
+    "Creamy",
+    "Frozen",
+    "Longdrink",
+    "Martini"
+  ],
+  Mood: [
+    "Relaxed",
+    "Upbeat",
+    "Romantic",
+    "Festive",
+    "Cozy",
+    "Adventurous"
+  ],
+  Occasion: [
+    "Party",
+    "Dinner",
+    "Beach",
+    "Casual",
+    "Date Night",
+    "Brunch"
+  ]
+};
+
+export default categoryData;
