e5864c4fb70eadf49c42d60e0aee3e309a1f87d4
Tony Miller <82892112+anthonypmiller11@users.noreply.github.com>
2025-03-21 00:32:24 -0600

    Update index.js

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,11 +1,11 @@
-export {default as Layout} from './layout/Layout';
-export {default as Header} from './layout/Header';
-export {default as Footer} from './layout/Footer';
-export {default as Home} from './home/Home';
-export {default as Popular} from './home/Popular';
-export {default as Stats} from './home/Stats';
-export {default as Cocktails} from './home/Cocktails';
-export {default as CocktailInfo} from './cocktail/CocktailInfo';
-export {default as Instructions} from './cocktail/Instructions';
-export {default as VideoTutorial} from './cocktail/VideoTutorial';
-export {default as SearchCocktails} from './SearchCocktails';
+export { default as Layout } from './layout/Layout';
+export { default as Header } from './layout/Header';
+export { default as Footer } from './layout/Footer';
+export { default as Home } from './home/Home';
+export { default as Popular } from './home/Popular';
+export { default as Stats } from './home/Stats';
+export { default as Cocktails } from './home/Cocktails';
+export { default as CocktailInfo } from './cocktail/CocktailInfo';
+export { default as Instructions } from './cocktail/Instructions';
+export { default as VideoTutorial } from './cocktail/VideoTutorial';
+export { default as ImageRotation } from './home/ImageRotation'; // ✅ NEW
